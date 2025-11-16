a43c7a72b200db6ae380364b14b0cead2e1807f0
bc990424 <65002008+bc990424@users.noreply.github.com>
2025-11-16 23:55:55 +0900

    Create utils.py

---
+++ geo/utils.py
@@ -0,0 +1,10 @@
+
+import math  # [cite: 444]
+
+def pythagoras(a, b):  # [cite: 451]
+    c = math.sqrt(a**2 + b**2)  # [cite: 454]
+    return c  # [cite: 458]
+
+def circle(r):
+    area = math.pi * r**2 
+    return area  # [cite: 470]
